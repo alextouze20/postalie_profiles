da8cfb18d8f7fc59db381a91c8299dbcbe776263
alex tz <alextouzepro@gmail.com>
2020-11-09 13:11:37 +0100

    start setup

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2020_11_09_115819) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "profiles", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.text "bio"
+    t.text "fullname"
+    t.text "address_street1"
+    t.text "address_street2"
+    t.integer "address_city"
+    t.text "address_province"
+    t.text "address_country"
+    t.text "link"
+    t.text "address_code"
+  end
 
 end
